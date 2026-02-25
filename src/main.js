87d877233e56fc0a5ed60f32bc4d472c46673d8c
Pedro do O <pedrodoo@gmail.com>
2026-02-25 16:30:07 +0000

    Add todo list HTML structure and styles

--- src/main.js
+++ src/main.js
@@ -1,17 +1,6 @@
 import './style.css'
 import { addTodo, toggleTodo, removeTodo, renderTodoList } from './todos.js'
 
-document.querySelector('#app').innerHTML = `
-  <div class="todo-app">
-    <h1>Todo List</h1>
-    <form class="todo-form" id="todo-form">
-      <input type="text" class="todo-form__input" id="todo-input" placeholder="What needs to be done?" autocomplete="off" />
-      <button type="submit" class="todo-form__submit">Add</button>
-    </form>
-    <ul class="todo-list" id="todo-list"></ul>
-  </div>
-`
-
 const form = document.getElementById('todo-form')
 const input = document.getElementById('todo-input')
 const listEl = document.getElementById('todo-list')
